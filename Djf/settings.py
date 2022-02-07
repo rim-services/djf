1ce5cab71dd323c985fb175667a42f820cee9079
Mohamed Beirouk <mbeirouck@gmail.com>
2022-02-07 21:53:27 +0000

    allowed host

--- Djf/settings.py
+++ Djf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5tyedp^qd289ra)xu#y4##&d95+*4z4q309a13govpi877(7lc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://beirouk-djf.herokuapp.com/']
 
 
 # Application definition
