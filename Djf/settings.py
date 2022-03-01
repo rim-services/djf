c24119f8a1aa8631b6f5cad1fbd4b00f51686941
Mohamed Beirouk <mbeirouck@gmail.com>
2022-03-01 14:54:34 +0000

    rest

--- Djf/settings.py
+++ Djf/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
     'myappdjf',
     'django_extensions',
+    
+    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
